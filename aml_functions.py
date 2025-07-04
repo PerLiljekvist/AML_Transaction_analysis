ef4539c9de631bbf06487d497274e90d6a4015c1
PerLiljekvist <60550105+PerLiljekvist@users.noreply.github.com>
2025-07-04 23:19:07 +0200

    Add files via upload
    Added separate file for functions. Just made sure it's runnable.  Move of functions at later stage.

---
+++ aml_functions.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import csv
+import networkx as nx
+from scipy import stats
+from datetime import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
+import io
+import os
+import numpy as np
+
+print("runnable it seems!")
