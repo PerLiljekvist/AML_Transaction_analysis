1be7ee7fef1971cc480bbecf0ac18c3140383e43
PerLiljekvist <60550105+PerLiljekvist@users.noreply.github.com>
2025-09-13 14:05:49 +0200

    Add files via upload
    Helper function to find accounts with the most number of transactions.

--- paths_and_stuff.py
+++ paths_and_stuff.py
@@ -1,2 +1,3 @@
 folderPath = "/Users/perliljekvist/Documents/Python/IBM_AML/Data/"
 filePath = "/Users/perliljekvist/Documents/Python/IBM_AML/Data/HI-Small_Trans.csv"
+#filePath = "/Users/perliljekvist/Documents/Python/IBM_AML/Data/LI-Small_Trans.csv"
