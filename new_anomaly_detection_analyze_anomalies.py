0fa01dbeda1c16c5a4e6ec9bde59c20c98647f28
PerLiljekvist <60550105+PerLiljekvist@users.noreply.github.com>
2026-01-21 07:27:47 +0100

    Add files via upload
    Added som seaborn plots.. Runnable although a bit slow.

--- new_anomaly_detection_analyze_anomalies.py
+++ new_anomaly_detection_analyze_anomalies.py
@@ -10,19 +10,38 @@ from paths_and_stuff import *
 import time
 import seaborn as sns
 import matplotlib.pyplot as plt
+sns.set_style(style='dark')
 
 start = time.time()
 PATH = create_new_folder(folderPath, datetime.now().strftime("%Y-%m-%d"))
 CSV_SEP     = ";"  
 
-INPUT_FILE  = PATH + "/tx_pre_model_with_account_context_pre_model.csv" 
+INPUT_FILE  = PATH + "/tx_with_pyod_anomalies.csv" 
 
 df = pd.read_csv(INPUT_FILE, sep=CSV_SEP)
+df = df.head(1000)
 # print(df.info())
+# quit()
 
-plt.figure(figsize=(10,8))
-sns.scatterplot(data=df, x="Amount Paid", y="Amount Received")
+fig, axes = plt.subplots(2, 2, figsize=(15, 5))
+fig.suptitle("Anomaly output lab")
+
+sns.histplot(data=df, x="iforest_score", ax=axes[0,0], kde=True)
+#axes[0,1].set_title("iforest_score")
+
+sns.histplot(data=df, x="lof_score", ax=axes[0,1], kde=True)
+#axes[0,1].set_title("lof_score")
+
+sns.boxplot(data=df, x="iforest_score", ax=axes[1,0])
+#axes[1,0].set_title("iforest_score")
+
+sns.boxplot(data=df, x="lof_score", ax=axes[1,1])
+#axes[1,1].set_title("lof_score")
+
+plt.tight_layout()
 plt.show()
+
+
 
 
 
